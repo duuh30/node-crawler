2f65c8932d48bd1e3d3932f352b1e62478f4230e
Eduardo Augusto <eduardoaugusto@mangue3.com>
2018-12-19 11:50:57 -0300

    identado

--- src/services/promotionService.js
+++ src/services/promotionService.js
@@ -5,7 +5,7 @@ const Promotion = mongoose.model('Promotion');
 var rp = require('request-promise');
 const pup = require('puppeteer');
 
-exports.save = async (req,res) => {
+exports.save = async (req, res) => {
     try {
         var options = {
             uri: 'https://www.hardmob.com.br/forums/407-Promocoes',
@@ -14,37 +14,37 @@ exports.save = async (req,res) => {
 
         const body = await rp.get(options);
         const $ = cheerio.load(body);
-                const baseUrl = "www.hardmob.com.br/";
-                const promotions = [];
-                $('.threadtitle a').each(async(i, el) => {
-                    const items = $(el).text().replace(/[`~!@#%�çÇ^&ã*()_|+\-=÷¿?;:'"<>\{\}\\\\/]/gi, '');
-                    const links = $(el).attr('href');
-                    promotions.push(
-                        {
-                            title: items,
-                            url: baseUrl + links
-                        }
-                    );
-
-
-
-                    const addPromotion = {
-                        title: items,
-                        url: baseUrl + links
-                    };
-
-                    const ModelPromotion = new Promotion(addPromotion);
-                     await ModelPromotion.save();
-
-                });
-            const json = [{
-                data: {
-                    message: "Promoções cadastradas com sucesso",
-                    status_error: false
+        const baseUrl = "www.hardmob.com.br/";
+        const promotions = [];
+        $('.threadtitle a').each(async (i, el) => {
+            const items = $(el).text().replace(/[`~!@#%�çÇ^&ã*()_|+\-=÷¿?;:'"<>\{\}\\\\/]/gi, '');
+            const links = $(el).attr('href');
+            promotions.push(
+                {
+                    title: items,
+                    url: baseUrl + links
                 }
-            }];
+            );
 
-            return json;
+
+
+            const addPromotion = {
+                title: items,
+                url: baseUrl + links
+            };
+
+            const ModelPromotion = new Promotion(addPromotion);
+            await ModelPromotion.save();
+
+        });
+        const json = [{
+            data: {
+                message: "Promoções cadastradas com sucesso",
+                status_error: false
+            }
+        }];
+
+        return json;
 
     } catch (err) {
         console.log(err);
