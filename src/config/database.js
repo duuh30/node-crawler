d5da1175215bcef124c6b33d4aae199ebc59f5d7
Eduardo Augusto <eduardoaugusto@mangue3.com>
2018-12-20 07:59:52 -0300

    nada

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,11 @@
 'use strict';
 const mongoose     = require('mongoose');
+const path = require('path');
+const { readdirSync } = require('fs');
 
 // CONNECT DATABASE MONGOOSE ROBO3T
 mongoose.connect('mongodb://duuh30:nanda123@ds042128.mlab.com:42128/node-crawler',{useNewUrlParser: true});
 // mongoose.connect('mongodb://localhost/noderestapi',{useNewUrlParser: true});
 
-// LOAD MODELS
+//LOAD MODELS
 const Promotion = require('./../models/promotion');
