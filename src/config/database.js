45ae7c3d9a43ab7a8da394b401db8a8becf30fb1
duuh30 <38956084+duuh30@users.noreply.github.com>
2019-01-23 09:30:08 -0300

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -4,7 +4,7 @@ const path = require('path');
 const { readdirSync } = require('fs');
 
 // CONNECT DATABASE MONGOOSE ROBO3T
-mongoose.connect('mongodb://duuh30:nanda123@ds042128.mlab.com:42128/node-crawler',{useNewUrlParser: true});
+mongoose.connect('mongodb://usermlab:passwordmlab@ds042128.mlab.com:42128/node-crawler',{useNewUrlParser: true});
 // mongoose.connect('mongodb://localhost/noderestapi',{useNewUrlParser: true});
 
 //LOAD MODELS
